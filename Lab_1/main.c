13cd18ab1f68bd593a757a1a033c0de388f63e00
Odin <enrique.rodriguez.513@gmail.com>
2022-01-21 23:36:03 -0800

    Lab#1 Fourth Commit
    1) Commented out Previous Codes
    2) Added IsVowel function with char aLetter argument
    3) Created if Statements if BRUTE FORCE vowels print vowel return 1
    BRUTE FORCE consonants print  consonant return 0
    special characters print error

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -3,50 +3,14 @@
  
 #include<stdio.h>
 
-int IsLeapYear(int aYear)
-
-
-{
-    /* No leap year before year 1752 */
-     if (aYear < 1752)
-        {
-            printf("\nYou Entered %d: There are no leap years before 1752.\n", aYear);
-        return 0;
-        }
+/* Start Variable Modifier Size (bytes) Function
+     
     
-    /* If year is divisible by 400, then year is a leap year */
-     else if (aYear % 400 == 0)
-        {
-            printf("\nYou entered %d: This is a leap year.\n", aYear);
-        return 1;
-        }
-        
-    /* Not a leap year if divisible by 100 */
-     else if (aYear % 100 == 0)
-        {
-            printf("\nYou entered %d: This is not a leap year.\n", aYear);
-        return 0;
-        }
-        
-    /* If year is divisible by 4, then year is a leap year */
-     else if (aYear % 4 == 0)
-        {
-            printf("\nYou entered %d: This is a leap year.\n", aYear);
-        return 1;
-        }
-        
-    /* All other years are not leap years */
-     else 
-        {
-            printf("\nYou entered %d: This is not a leap year.\n", aYear);
-        return 0;
-        }
-}
-/*
-void SizeOfVariables(void)
+    void SizeOfVariables(void)
 {
         /* Declare Variables these are local within this function */
-        /*char c;
+        /*
+        char c;
         short s;
         int i;
         unsigned int ui;unsigned long int ul;
@@ -62,46 +26,101 @@ void SizeOfVariables(void)
         printf("\n\nSize of Float: %zu bytes\n", sizeof(f));
         printf("\n\nSize of Double: %zu bytes\n", sizeof(d));
         printf("\n\nSize of Long Double: %zu bytes\n", sizeof(ld));
-        printf("\n\nSize of void: %zu bytes\n", sizeof(*ptr));
-      
+        printf("\n\nSize of void: %zu bytes\n", sizeof(*ptr));      
 }     
-        */
+End of Variable Modifier Size (bytes) Function */
+
+
+/* Start of Is Leap Year Function
     
+     
+     int IsLeapYear(int aYear)
+{
+    /* No leap year before year 1752
+     if (aYear < 1752)
+        {
+            printf("\nYou Entered %d: There are no leap years before 1752.\n", aYear);
+        return 0;
+        }
+    
+    /* If year is divisible by 400, then year is a leap year
+     else if (aYear % 400 == 0)
+        {
+            printf("\nYou entered %d: This is a leap year.\n", aYear);
+        return 1;
+        }
+        
+    /* Not a leap year if divisible by 100
+     else if (aYear % 100 == 0)
+        {
+            printf("\nYou entered %d: This is not a leap year.\n", aYear);
+            return 0;
+        }
+        
+    /* If year is divisible by 4, then year is a leap year
+     else if (aYear % 4 == 0)
+        {
+            printf("\nYou entered %d: This is a leap year.\n", aYear);
+            return 1;
+        }
+        
+    /* All other years are not leap years
+     else 
+        {
+            printf("\nYou entered %d: This is not a leap year.\n", aYear);
+            return 0;
+        }
+}
+End of Is Leap Year Function */    
+
+
+/* Start of Is Vowel Function BRUTE FORCE METHOD */
+
+
+    int IsVowel(char aLetter) {
+        if (aLetter == 'a' ||aLetter == 'e'||aLetter == 'i'||aLetter == 'o'||aLetter == 'u'||aLetter == 'A'||aLetter == 'E'||aLetter == 'I'
+            ||aLetter == 'O'||aLetter == 'U'){
+            printf("\nYou entered %c: This is a vowel!\n", aLetter); 
+            return 1;
+    }
+        else if (aLetter == 'b' ||aLetter == 'c'||aLetter == 'd'||aLetter == 'f'||aLetter == 'g'||aLetter == 'h'||aLetter == 'j'
+            ||aLetter == 'k'||aLetter == 'l'||aLetter == 'm'||aLetter == 'n'||aLetter == 'p'||aLetter == 'q'||aLetter == 'r'||aLetter == 's'
+            ||aLetter == 't'||aLetter == 'v'||aLetter == 'w'||aLetter == 'x'||aLetter == 'y'||aLetter == 'z'||aLetter == 'B' ||aLetter == 'C'
+            ||aLetter == 'D'||aLetter == 'F'||aLetter == 'G'||aLetter == 'H'||aLetter == 'J'
+            ||aLetter == 'K'||aLetter == 'L'||aLetter == 'M'||aLetter == 'N'||aLetter == 'P'||aLetter == 'Q'||aLetter == 'R'||aLetter == 'S'
+            ||aLetter == 'T'||aLetter == 'V'||aLetter == 'W'||aLetter == 'X'||aLetter == 'Y'||aLetter == 'Z'){
+            printf("\nYou entered %c: This is a consonant!\n", aLetter);
+            return 0;
+    }
+        else{
+            printf("\nError!! allyourbasearebelongtous ", aLetter);
+            return 0;
+        }
+}
 
 int main()
 {
-        int aNumber;
+ 
+/* Start of User Input and Output (scanf)and ASCII 
+       
 
-        printf("Enter a year:");
-        scanf("%d", &aNumber);
-        
-        int myvalue = IsLeapYear(aNumber);
-        
-        printf("This function returns %d.\n", myvalue);
-        
-        /* Calling SizeOfVariables() from within main */
-        /* SizeOfVariables();
-        
-        /*int num1, num2;
+        int num1, num2;
         float fraction;
         char character;
-        char str[50];    //char array of size 50
-        */
+        char str[50];   /* char string size of 50 
         
-        // Taking integer as input from user
-        
-        /*
+        /* Taking integer as input from user
         printf("Enter two numbers\n");
         scanf(" %d%i", &num1,  &num2);
         printf("\n\nThe two numbers You have entered are %d and %i\n\n", num1, num2); 
-        */
+        
         
         //Taking float or fraction as input from the user
         /*
         printf("\n\nEnter a Decimal number\n");
         scanf(" %f", &fraction);
         printf("\n\nThe float or fraction that you have entered is %f", fraction);
-        */
+        
          
         //Taking Character as input from the user
         
@@ -115,12 +134,48 @@ int main()
         gets(str);
         printf("\n\nWelcome to ECEGR 2020 %s\n\n\n", str);
         printf("\n\n\t\tCoding is Fun !\n\n\n");
-        */
+        
     
         
         /*printf("Press Enter to quit");
-        scanf("%c", &character);
-        */
+        scanf("%c", &character); 
+End of User Input and Output (scanf) and ASCII main        */
+    
+
+/* Start of Variable Modifier Size (bytes) main
+        
+        
+    /* Calling SizeOfVariables() from within main 
+        SizeOfVariables();
+End of Variable Modifier Size (bytes) main */ 
+
+ 
+/* Start of Is Leap Year main        
+        
+    
+        int aNumber;
+
+        printf("Enter a year:");
+        scanf("%d", &aNumber);
+        
+        int myvalue = IsLeapYear(aNumber);
+        
+        printf("This function returns %d.\n", myvalue);
+End of Is Leap Year main */  
+
+
+/* Start of Is Vowel main */
+    
+        char userinputletter;
+        
+        printf("Enter an alphabet letter:");
+        scanf("%c", &userinputletter);
+
+        int logictest = IsVowel(userinputletter);
+        
+        
+
+        printf("\nThis function returns %d.\n\n", logictest);
         
         return 0;
 }
