54bce9bc578eea00e947aaf952c9c481f9ee0588
Odin <enrique.rodriguez.513@gmail.com>
2022-02-02 16:45:21 -0800

    Lab_2 seventh Commit
    Finished ReverseIt Function (works)

--- Lab_2/ReverseIt.c
+++ Lab_2/ReverseIt.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 void ReverseIt(void){
+    float userinputvalues[5];
+    int i;
     
-    return 1;
+        for(i = 0; i < 5; i++){
+            printf("Enter five floating point values: ");
+            scanf("%f" , &userinputvalues[i]);
+        }
+        
+        for (i = 0; i < 5; i++){
+        printf("%f\n", userinputvalues[5-1-i]);
+        
+        }
+        
+        scanf("NOTHING");
+        return 0;
+}
+          
+void main(){
+    
+        ReverseIt();
+    
+    return 0;
 }
 
