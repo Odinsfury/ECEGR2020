16772a3d4fdcd268b7e2452b4220d79dedda028b
Odin <enrique.rodriguez.513@gmail.com>
2022-03-21 16:55:41 -0700

    Lab 6 Commit Again
    updated to handle string names not char with pointers

--- Lab_6/Class.cpp
+++ Lab_6/Class.cpp
@@ -8,16 +8,16 @@ class Student
 {
 private:
     
-    int          sID;
-    char *sfirstName;
-    char  *slastName;
-    float       sGPA;
+    int           sID;
+    string sfirstName;
+    string  slastName;
+    float        sGPA;
 
 
 public:
     Student();
-    Student(int sID, const char *sfirstName, const char *slastName, float sGPA);
-    Student(const char *sfirstName, const char *slastName);
+    Student(int sID, const string sfirstName, const string slastName, float sGPA);
+    Student(const string sfirstName, const string slastName);
     Student(Student &sinit);
     ~Student();
     
@@ -28,13 +28,13 @@ public:
     }
     
     // Get STUDENT function for First Name
-    const char *getsfirstName()
+    const string getsfirstName()
     {
         return sfirstName;
     }
     
     // Get STUDENT function for Last Name
-    const char *getslastName()
+    const string getslastName()
     {
         return slastName;
     }
@@ -52,19 +52,15 @@ public:
     }
     
     // Set STUDENT function for first Name
-    void setsfirstName(char* sfirstName)
+    void setsfirstName(string sfirstName)
     {
-        int lensFirst = strlen(sfirstName) + 1;
-        this->sfirstName = new char[lensFirst];
-        strcpy(this->sfirstName, sfirstName);
+        this->sfirstName = sfirstName;
     }
     
     // Set STUDENT function for last Name
-    void setslastName(char* slastName)
+    void setslastName(string slastName)
     {
-        int lensLast = strlen(slastName)  + 1;
-        this->slastName = new char[lensLast];
-        strcpy(this->slastName, slastName);  
+        this->slastName = slastName;  
     }
     
     // Set STUDENT function for GPA
@@ -88,33 +84,25 @@ public:
 Student::Student()
 {
     sID        = NULL;
-    sfirstName = NULL;
-    slastName  = NULL;
+    sfirstName = "";
+    slastName  = "";
     sGPA       = NULL;
 }
 
 // STUDENT Constructor w 4 inputs
-Student::Student(int sID, const char *sfirstName, const char *slastName, float sGPA)
+Student::Student(int sID, const string sfirstName, const string slastName, float sGPA)
 {
-    int lensFirst = strlen(sfirstName) + 1;
-    int lensLast = strlen(slastName)  + 1;
     this->sID = sID;
     this->sGPA = sGPA;
-    this->sfirstName = new char[lensFirst];
-    this->slastName = new char[lensLast];
-    strcpy(this->sfirstName, sfirstName);
-    strcpy(this->slastName, slastName);
+    this->sfirstName = sfirstName;
+    this->slastName = slastName;
 }
 
 // STUDENT Constructo with only first and last name
-Student::Student(const char *sfirstName, const char *slastName)
+Student::Student(const string sfirstName, const string slastName)
 {
-    int lensFirst = strlen(sfirstName) + 1;
-    int lensLast = strlen(slastName)  + 1;
-    this->sfirstName = new char[lensFirst];
-    this->slastName = new char[lensLast];
-    strcpy(this->sfirstName, sfirstName);
-    strcpy(this->slastName, slastName);
+    this->sfirstName = sfirstName;
+    this->slastName = slastName;
 }
 
 // STUDENT Copy Constructor
@@ -123,17 +111,12 @@ Student::Student(Student &sinit)
     // set first name
     setsfirstName( sinit.sfirstName );
     {
-        int lensFirst = strlen(sfirstName) + 1;
-        sfirstName = new char[lensFirst];
         sfirstName = sinit.sfirstName;
     }
     
     // set last name
     setslastName( sinit.slastName );
     {
-        // allocate and copy string
-        int lensLast = strlen(slastName) + 1;
-        slastName = new char[lensLast];
         slastName = sinit.slastName;
     }
     
@@ -146,7 +129,7 @@ Student::Student(Student &sinit)
 Student::~Student()
 {
     cout << "Destructor called: "<< endl;
-    if( sfirstName )
+   /* if( sfirstName )
     {
         // free memory
         free(sfirstName);
@@ -156,7 +139,7 @@ Student::~Student()
     {
         // free memory
         free( slastName );
-    }
+    }*/
     
 }
 
@@ -166,8 +149,8 @@ class Professor
 private:
     
     int          pID;
-    char *pfirstName;
-    char  *plastName;
+    string pfirstName;
+    string plastName;
     float    pSalary;
     bool    pTenured;
 //Department department;
@@ -175,8 +158,8 @@ private:
 
 public:
     Professor();
-    Professor(int pID, const char *pfirstName, const char *plastName, float pSalary, bool pTenured);
-    Professor(const char *pfirstName, const char *plastName);
+    Professor(int pID, const string pfirstName, const string plastName, float pSalary, bool pTenured);
+    Professor(const string pfirstName, const string plastName);
     Professor(Professor &pinit);
     ~Professor();
     
@@ -187,13 +170,13 @@ public:
     }
     
     // Get function for First Name
-    const char *getpfirstName()
+    const string getpfirstName()
     {
         return pfirstName;
     }
     
     // Get function for Last Name
-    const char *getplastName()
+    const string getplastName()
     {
         return plastName;
     }
@@ -218,19 +201,15 @@ public:
     }
     
     // Set function for first Name
-    void setpfirstName(char* pfirstName)
+    void setpfirstName(string pfirstName)
     {
-        int lenpFirst = strlen(pfirstName) + 1;
-        this->pfirstName = new char[lenpFirst];
-        strcpy(this->pfirstName, pfirstName);
+        this->pfirstName = pfirstName;
     }
     
     // Set function for last Name
-    void setplastName(char* plastName)
+    void setplastName(string plastName)
     {
-        int lenpLast = strlen(plastName)  + 1;
-        this->plastName = new char[lenpLast];
-        strcpy(this->plastName, plastName);  
+        this->plastName = plastName;  
     }
     
     // Set function for Salary
@@ -260,35 +239,28 @@ public:
 Professor::Professor()
 {
     pID        = NULL;
-    pfirstName = NULL;
-    plastName  = NULL;
+    pfirstName = "";
+    plastName  = "";
     pSalary    = NULL;
     pTenured   = NULL;
 }
 
 
-Professor::Professor(int pID, const char *pfirstName, const char *plastName, float pSalary, bool pTenured)
+Professor::Professor(int pID, const string pfirstName, const string plastName, float pSalary, bool pTenured)
 {
     this->pID = pID;
-    int lenpFirst = strlen(pfirstName) + 1;
-    int lenpLast = strlen(plastName)  + 1;
-    this->pfirstName = new char[lenpFirst];
-    this->plastName = new char[lenpLast];
-    strcpy(this->pfirstName, pfirstName);
-    strcpy(this->plastName, plastName);
+    this->pfirstName = pfirstName;
+    this->plastName = plastName;
     this->pSalary = pSalary;
     this->pTenured = pTenured;
     
 }
 
-Professor::Professor(const char *pfirstName, const char *plastName)
+Professor::Professor(const string pfirstName, const string plastName)
 {
-    int lenpFirst = strlen(pfirstName) + 1;
-    int lenpLast = strlen(plastName)  + 1;
-    this->pfirstName = new char[lenpFirst];
-    this->plastName = new char[lenpLast];
-    strcpy(this->pfirstName, pfirstName);
-    strcpy(this->plastName, plastName);
+   
+    this->pfirstName = pfirstName;
+    this->plastName = plastName;
 }
 
 // Copy Constructor
@@ -297,8 +269,6 @@ Professor::Professor(Professor &pinit)
     // set first name
     setpfirstName( pinit.pfirstName );
     {
-        int lenpFirst = strlen(pfirstName) + 1;
-        pfirstName = new char[lenpFirst];
         pfirstName = pinit.pfirstName;
     }
     
@@ -306,8 +276,6 @@ Professor::Professor(Professor &pinit)
     setplastName( pinit.plastName );
     {
         // allocate and copy string
-        int lenpLast = strlen(plastName) + 1;
-        plastName = new char[lenpLast];
         plastName = pinit.plastName;
     }
     
@@ -321,7 +289,7 @@ Professor::Professor(Professor &pinit)
 Professor::~Professor()
 {
     cout << "Destructor called: "<< endl;
-    if( pfirstName )
+    /*if( pfirstName )
     {
         // free memory
         free(pfirstName);
@@ -331,7 +299,7 @@ Professor::~Professor()
     {
         // free memory
         free( plastName );
-    }
+    }*/
     
 }
 
@@ -341,14 +309,14 @@ class Employee
 private:
     
     int          eID;
-    char *efirstName;
-    char  *elastName;
+    string efirstName;
+    string elastName;
     float    eSalary;
     
 public:
     Employee();
-    Employee(int eID, const char*efirstName, const char *elastName, float eSalary);
-    Employee(const char *efirstName, const char *elastName);
+    Employee(int eID, const string efirstName, const string elastName, float eSalary);
+    Employee(const string efirstName, const string elastName);
     Employee(Employee &einit);
     ~Employee();
     
@@ -359,12 +327,12 @@ public:
     }
     
     // Get function for Employee First Name
-    const char *getefirstName()
+    const string getefirstName()
     {
         return efirstName;
     }
     
-    const char *getelastName()
+    const string getelastName()
     {
         return elastName;
     }
@@ -382,19 +350,15 @@ public:
     }
     
     // Set Employee First Name
-    void setefirstName(char* efirstName)
+    void setefirstName(string efirstName)
     {
-        int leneFirst = strlen(efirstName) + 1;
-        this->efirstName = new char[leneFirst];
-        strcpy(this->efirstName, efirstName);
+        this->efirstName = efirstName;
     }
     
     // Set Employee Last Name
-    void setelastName(char* elastName)
+    void setelastName(string elastName)
     {
-        int leneLast = strlen(elastName) + 1;
-        this->elastName = new char[leneLast];
-        strcpy(this->elastName, elastName);
+        this->elastName = elastName;
     }
     
     
@@ -418,33 +382,25 @@ public:
 Employee::Employee()
 {
     eID        = NULL;
-    efirstName = NULL;
-    elastName  = NULL;
+    efirstName = "";
+    elastName  = "";
     eSalary    = NULL;
 }
 
 
-Employee::Employee(int eID, const char *efirstName, const char *elastName, float eSalary)
+Employee::Employee(int eID, const string efirstName, const string elastName, float eSalary)
 {
     this->eID = eID;
-    int leneFirst = strlen(efirstName) + 1;
-    int leneLast = strlen(elastName)  + 1;
-    this->efirstName = new char[leneFirst];
-    this->elastName = new char[leneLast];
-    strcpy(this->efirstName, efirstName);
-    strcpy(this->elastName, elastName);
+    this->efirstName = efirstName;
+    this->elastName = elastName;
     this->eSalary = eSalary;
     
 }
 
-Employee::Employee(const char *efirstName, const char *elastName)
+Employee::Employee(const string efirstName, const string elastName)
 {
-    int leneFirst = strlen(efirstName) + 1;
-    int leneLast = strlen(elastName)  + 1;
-    this->efirstName = new char[leneFirst];
-    this->elastName = new char[leneLast];
-    strcpy(this->efirstName, efirstName);
-    strcpy(this->elastName, elastName);
+    this->efirstName = efirstName;
+    this->elastName = elastName;
 }
 
 // Copy Constructor
@@ -453,17 +409,12 @@ Employee::Employee(Employee &einit)
     // set first name
     setefirstName( einit.efirstName );
     {
-        int leneFirst = strlen(efirstName) + 1;
-        efirstName = new char[leneFirst];
         efirstName = einit.efirstName;
     }
     
     // set last name
     setelastName( einit.elastName );
     {
-        // allocate and copy string
-        int leneLast = strlen(elastName) + 1;
-        elastName = new char[leneLast];
         elastName = einit.elastName;
     }
     
@@ -476,7 +427,7 @@ Employee::Employee(Employee &einit)
 Employee::~Employee()
 {
     cout << "Destructor called: "<< endl;
-    if( efirstName )
+    /*if( efirstName )
     {
         // free memory
         free(efirstName);
@@ -486,7 +437,7 @@ Employee::~Employee()
     {
         // free memory
         free( elastName );
-    }
+    }*/
     
 }
 
